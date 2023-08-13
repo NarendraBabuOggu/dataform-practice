554a02c52b3c294bbe66e3c888a673907f9afcd8
NARENDRABABUOGGU <narendrababuoggu393@gmail.com>
2023-08-13 16:40:03 +0000

    Added code for dimension and fact load

---
+++ includes/column_descriptions.js
@@ -0,0 +1,13 @@
+const order_status = `Order Status. Can be One of the following values: 
+
+placed         - Order placed, not yet shipped                    
+shipped        - Order has been shipped, not yet been delivered   
+completed      - Order has been received by customers             
+return pending - Customer indicated they want to return this item 
+returned       - Item has been returned                           
+
+`;
+
+module.exports = {
+    order_status
+}
